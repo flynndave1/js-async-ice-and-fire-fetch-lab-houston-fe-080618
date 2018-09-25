2f6f31637f894452bf57b1e3ed35643575b660f6
david m flynn <flynndave1@msn.com>
2018-09-25 20:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => console.log(json)) 
-  renderBooks(json);
+ json => renderBooks(json);
   // .then( json => renderBooks(json));
 
     }
