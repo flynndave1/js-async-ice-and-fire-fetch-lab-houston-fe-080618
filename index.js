b8b5ef5ee842f14c31a7374b8c13ba24f5043cbe
david m flynn <flynndave1@msn.com>
2018-09-25 20:03:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
+
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
-}
+  //write fetch request to the Game of Thrones 
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  // .then(json => console.log(json) )
+  .then( json => renderBooks(json));
+
+    }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    const h4 = document.createElement('h2')
+    h4.innerHTML = `<h4>${book.name}</h4>`
+    main.appendChild(h4)
   })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+})
+
+
+function fetchHouses() {
+  //write fetch request to the Game of Thrones 
+  fetch('https://anapioficeandfire.com/api/houses')
+  .then(resp => resp.json())
+  // .then(json => console.log(json()))
+  .then( json => renderHouses(json()));
+  // h2 = document.createElement('h2')
+  // h2.innerHTML = `<h2>${houses[4]}`;
+    }
+
+function renderHouses(json) {
+  const main = document.querySelector('main')
+  json.forEach(houses => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${houses.name}</h2>`;
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchHouses()
 })
