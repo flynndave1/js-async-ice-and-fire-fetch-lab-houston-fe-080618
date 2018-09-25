23c59d76849be9771cabc9df0decfec1a18d6fc1
david m flynn <flynndave1@msn.com>
2018-09-25 20:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,10 +41,11 @@ function fetchHouses() {
 // }
 function renderHouses(json) {
   const main = document.querySelector('main')
-  json.forEach(houses => {
-    const h5 = document.createElement('h5');
-    h5.innerHTML = `<h5>${houses[4]}</h5>`;
-    main.appendChild(h5)
+  
+  // json.forEach(houses => {
+  //   const h5 = document.createElement('h5');
+  //   h5.innerHTML = `<h5>${houses.name[4]}</h5>`;
+  //   main.appendChild(h5)
   })
 }
 
